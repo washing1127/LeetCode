7dcf7cdd0de83205f0c7d32af61e26df982094df
washing1127 <1014585392@qq.com>
2021-09-18 10:22:26 +0800

    add 0292.py 0292.cpp

---
+++ Solutions/0292/0292.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2021/9/18 10:20
+# File: 0292.py
+# Desc: 
+
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        return n % 4 != 0

2de8462a13ecd2a6202e85483166084b0113a400
washing1127 <1014585392@qq.com>
2021-09-25 21:06:26 +0800

    add 0583.py

---
+++ Solutions/0583/0583.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2021/9/26 10:16
+# File: 0583.py
+# Desc: 
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        lcs = dp[m][n]
+        return m - lcs + n - lcs
+

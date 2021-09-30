c945e76f7f3e88fa5c042258904825fcf2939466
washing1127 <1014585392@qq.com>
2021-09-30 09:54:54 +0800

    add 0223.py 0223.cpp

---
+++ Solutions/0223/0223.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2021/9/30 9:53
+# File: 0223.py
+# Desc: 
+
+
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        area1 = (ax2 - ax1) * (ay2 - ay1)
+        area2 = (bx2 - bx1) * (by2 - by1)
+        overlapWidth = min(ax2, bx2) - max(ax1, bx1)
+        overlapHeight = min(ay2, by2) - max(ay1, by1)
+        overlapArea = max(overlapWidth, 0) * max(overlapHeight, 0)
+        return area1 + area2 - overlapArea

eff0d942608ad65b6921191d2bdac8adecf61ae8
washing1127 <1014585392@qq.com>
2021-10-25 13:42:40 +0800

    add 0240.py

---
+++ Solutions/0240/0240.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2021/10/24 13:39
+# File: 0240.py
+# Desc: 
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        x, y = 0, n - 1
+        while x < m and y >= 0:
+            if matrix[x][y] == target:
+                return True
+            if matrix[x][y] > target:
+                y -= 1
+            else:
+                x += 1
+        return False
+

36ad8409c5b5d94540774b3cb2615b5baaab5096
washing1127 <1014585392@qq.com>
2021-11-11 11:57:05 +0800

    add 0629.py

--- Solutions/0629/0629.py
+++ Solutions/0629/0629.py
@@ -1,18 +1,20 @@
 # -*- coding:utf-8 -*-
 # Author: washing
-# DateTime: 2021/5/20 10:53
+# DateTime: 2021/11/11 11:54
 # File: 0629.py
 # Desc: 
-class Solution:
-    def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        c = collections.Counter(words)
-        keys = list(c.keys())
-        keys.sort(reverse=True)
-        keys.sort(key=lambda i: c[i])
-        li = []
-        for i in range(k):
-            key = keys[-1-i]
-            v = key
-            li.append(v)
 
-        return li
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        mod = 10**9 + 7
+        
+        f = [1] + [0] * k
+        for i in range(1, n + 1):
+            g = [0] * (k + 1)
+            for j in range(k + 1):
+                g[j] = (g[j - 1] if j - 1 >= 0 else 0) - (f[j - i] if j - i >= 0 else 0) + f[j]
+                g[j] %= mod
+            f = g
+        
+        return f[k]
+

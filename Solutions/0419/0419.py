6953e9fab5e55d23f887cb9d1709830f5029c21b
washing1127 <1014585392@qq.com>
2021-12-18 15:28:30 +0800

    add 0419.py

---
+++ Solutions/0419/0419.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2021/12/18 16:51
+# File: 0419.py
+# Desc: CV
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        ans = 0
+        m, n = len(board), len(board[0])
+        for i, row in enumerate(board):
+            for j, ch in enumerate(row):
+                if ch == 'X':
+                    row[j] = '.'
+                    for k in range(j + 1, n):
+                        if row[k] != 'X':
+                            break
+                        row[k] = '.'
+                    for k in range(i + 1, m):
+                        if board[k][j] != 'X':
+                            break
+                        board[k][j] = '.'
+                    ans += 1
+        return ans
+

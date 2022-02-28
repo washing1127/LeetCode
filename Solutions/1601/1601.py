8da995615c8301c87e4ff431c34f7a0d4b8b9873
washing1127 <1014585392@qq.com>
2022-02-28 11:05:10 +0800

    add 1601.py

---
+++ Solutions/1601/1601.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/2/28 11:03
+# File: 1601.py
+# Desc: CV
+
+
+class Solution:
+    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+        delta = [0] * n
+        ans, cnt, zero = 0, 0, n
+
+        def dfs(pos: int) -> None:
+            nonlocal ans, cnt, zero
+            if pos == len(requests):
+                if zero == n:
+                    ans = max(ans, cnt)
+                return
+
+            # 不选 requests[pos]
+            dfs(pos + 1)
+
+            # 选 requests[pos]
+            z = zero
+            cnt += 1
+            x, y = requests[pos]
+            zero -= delta[x] == 0
+            delta[x] -= 1
+            zero += delta[x] == 0
+            zero -= delta[y] == 0
+            delta[y] += 1
+            zero += delta[y] == 0
+            dfs(pos + 1)
+            delta[y] -= 1
+            delta[x] += 1
+            cnt -= 1
+            zero = z
+
+        dfs(0)
+        return ans

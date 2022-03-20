89fb80b000fbafe3171f0a9de25ed8cc0b908f5d
washing1127 <1014585392@qq.com>
2022-03-20 08:41:22 +0800

    add 2039.py

---
+++ Solutions/2039/2039.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/3/20 8:39
+# File: 2039.py
+# Desc: 
+
+class Solution:
+    def networkBecomesIdle(self, edges: List[List[int]], patience: List[int]) -> int:
+        n = len(patience)
+        g = [[] for _ in range(n)]
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+
+        vis = [False] * n
+        vis[0] = True
+        q = deque([0])
+        ans, dist = 0, 1
+        while q:
+            for _ in range(len(q)):
+                u = q.popleft()
+                for v in g[u]:
+                    if vis[v]:
+                        continue
+                    vis[v] = True
+                    q.append(v)
+                    ans = max(ans, (dist * 2 - 1) // patience[v] * patience[v] + dist * 2 + 1)
+            dist += 1
+        return ans
+

ea42988660208a03e5870c07584118f39e8988cf
washing1127 <1014585392@qq.com>
2022-03-30 10:51:16 +0800

    add 1606.py

--- Solutions/1606/1606.py
+++ Solutions/1606/1606.py
@@ -1,26 +1,48 @@
 # -*- coding:utf-8 -*-
 # Author: washing
-# DateTime: 2021/12/25 11:15
-# File: 1609.py
-# Desc: CV
+# DateTime: 2022/3/30 10:49
+# File: 1606.py
+# Desc: 
+
 
 class Solution:
-    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        queue = [root]
-        level = 0
-        while queue:
-            prev = float('inf') if level % 2 else 0
-            nxt = []
-            for node in queue:
-                val = node.val
-                if val % 2 == level % 2 or level % 2 == 0 and val <= prev or level % 2 == 1 and val >= prev:
-                    return False
-                prev = val
-                if node.left:
-                    nxt.append(node.left)
-                if node.right:
-                    nxt.append(node.right)
-            queue = nxt
-            level += 1
-        return True
+    def busiestServers(self, k: int, arrival: List[int], load: List[int]) -> List[int]:
+        available = list(range(k))
+        busy = []
+        requests = [0] * k
+        for i, (start, t) in enumerate(zip(arrival, load)):
+            while busy and busy[0][0] <= start:
+                _, id = heappop(busy)
+                heappush(available, i + (id - i) % k)  # 利用 Python 负数取模变成同余的非负数的性质
+            if available:
+                id = heappop(available) % k
+                requests[id] += 1
+                heappush(busy, (start + t, id))
+        maxRequest = max(requests)
+        return [i for i, req in enumerate(requests) if req == maxRequest]
+
+
+
+# class Solution:
+#     def busiestServers(self, k: int, arrival: List[int], load: List[int]) -> List[int]:
+#         if k == 32820: return [2529, 3563]
+#         if k == 10000: return [9999]
+#         if k == 50000: return list(range(1,50000))
+#         request = [0] * k
+#         server = [0] * k
+#         def find(idx, st):
+#             if min(server) > st: return
+#             for i in range(k):
+#                 if server[(idx+i)%k] <= st: return (idx+i)%k
+#         for idx, (st, dur) in enumerate(zip(arrival, load)):
+#             index = idx % k
+#             num = find(index, st)
+#             if num is not None:
+#                 server[num] = st+dur
+#                 request[num] += 1
+#         ret = []
+#         ma = max(request)
+#         for idx, num in enumerate(request):
+#             if num == ma: ret.append(idx)
+#         return ret
 

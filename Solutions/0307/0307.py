5b164ade6fdc877812ba284474467a82d8b94a61
washing1127 <1014585392@qq.com>
2022-04-04 11:08:50 +0800

    add 0307.py

---
+++ Solutions/0307/0307.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/4/4 14:09
+# File: 0307.py
+# Desc: 
+
+class NumArray:
+    def __init__(self, nums: List[int]):
+        n = len(nums)
+        size = int(n ** 0.5)
+        sums = [0] * ((n + size - 1) // size)  # n/size 向上取整
+        for i, num in enumerate(nums):
+            sums[i // size] += num
+        self.nums = nums
+        self.sums = sums
+        self.size = size
+
+    def update(self, index: int, val: int) -> None:
+        self.sums[index // self.size] += val - self.nums[index]
+        self.nums[index] = val
+
+    def sumRange(self, left: int, right: int) -> int:
+        m = self.size
+        b1, b2 = left // m, right // m
+        if b1 == b2:  # 区间 [left, right] 在同一块中
+            return sum(self.nums[left:right + 1])
+        return sum(self.nums[left:(b1 + 1) * m]) + sum(self.sums[b1 + 1:b2]) + sum(self.nums[b2 * m:right + 1])
+

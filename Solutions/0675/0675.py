ecfab32c02847a8c8bb6aa9b2126b6fcdf85668b
washing1127 <1014585392@qq.com>
2022-05-23 11:09:41 +0800

    add 0675.py

---
+++ Solutions/0675/0675.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/5/23 10:30
+# File: 0675.py
+# Desc: CV
+
+class Solution:
+    def cutOffTree(self, forest: List[List[int]]) -> int:
+        def bfs(sx: int, sy: int, tx: int, ty: int) -> int:
+            m, n = len(forest), len(forest[0])
+            q = deque([(0, sx, sy)])
+            vis = {(sx, sy)}
+            while q:
+                d, x, y = q.popleft()
+                if x == tx and y == ty:
+                    return d
+                for nx, ny in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
+                    if 0 <= nx < m and 0 <= ny < n and forest[nx][ny] and (nx, ny) not in vis:
+                        vis.add((nx, ny))
+                        q.append((d + 1, nx, ny))
+            return -1
+
+        trees = sorted((h, i, j) for i, row in enumerate(forest) for j, h in enumerate(row) if h > 1)
+        ans = preI = preJ = 0
+        for _, i, j in trees:
+            d = bfs(preI, preJ, i, j)
+            if d < 0:
+                return -1
+            ans += d
+            preI, preJ = i, j
+        return ans
+

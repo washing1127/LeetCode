2b4833cad92ff8acb2eec8e9d9a499adb96cdc84
washing1127 <1014585392@qq.com>
2022-06-15 10:49:24 +0800

    add 0719.py

---
+++ Solutions/0719/0719.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/6/15 10:39
+# File: 0719.py
+# Desc: 
+
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        def count(mid: int) -> int:
+            cnt = 0
+            for j, num in enumerate(nums):
+                i = bisect_left(nums, num - mid, 0, j)
+                cnt += j - i
+            return cnt
+
+        nums.sort()
+        return bisect_left(range(nums[-1] - nums[0]), k, key=count)

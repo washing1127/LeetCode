d49fd908277ddfad419aef4a28a63cc25cd4b5f3
washing1127 <1014585392@qq.com>
2022-06-29 10:22:25 +0800

    add 0535.c 0535.py

---
+++ Solutions/0535/0535.c
@@ -0,0 +1,22 @@
+/***************************************************************************************************
+ * Copyright: washing
+ * FileName: 0535.c
+ * Author: washing
+ * Version: 1.0
+ * Date: 2022-6-29 10:20:45
+***************************************************************************************************/
+
+
+/** Encodes a URL to a shortened URL. */
+char* encode(char* longUrl) {
+    return longUrl;
+}
+
+/** Decodes a shortened URL to its original URL. */
+char* decode(char* shortUrl) {
+    return shortUrl;
+}
+
+// Your functions will be called as such:
+// char* s = encode(s);
+// decode(s);

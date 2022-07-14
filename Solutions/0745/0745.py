63ecd16f172ccefd66642d4c791d10625b0fca53
washing1127 <1014585392@qq.com>
2022-07-14 09:59:26 +0800

    add 0745.py

---
+++ Solutions/0745/0745.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/7/14 9:49
+# File: 0745.py
+# Desc: CV
+
+class WordFilter:
+
+    def __init__(self, words: List[str]):
+        self.d = {}
+        for i, word in enumerate(words):
+            m = len(word)
+            for prefixLength in range(1, m + 1):
+                for suffixLength in range(1, m + 1):
+                    self.d[word[:prefixLength] + '#' + word[-suffixLength:]] = i
+
+
+    def f(self, pref: str, suff: str) -> int:
+        return self.d.get(pref + '#' + suff, -1)
+

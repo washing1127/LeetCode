532bf4f886b9a18e66761eefb5491f9c950f8164
washing1127 <1014585392@qq.com>
2022-08-08 10:49:43 +0800

    add 0761.py

---
+++ Solutions/0761/0761.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/8/8 11:00
+# File: 0761.py
+# Desc: CV
+
+
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+        if len(s) <= 2:
+            return s
+        
+        cnt = left = 0
+        subs = list()
+
+        for i, ch in enumerate(s):
+            if ch == "1":
+                cnt += 1
+            else:
+                cnt -= 1
+                if cnt == 0:
+                    subs.append("1" + self.makeLargestSpecial(s[left+1:i]) + "0")
+                    left = i + 1
+        
+        subs.sort(reverse=True)
+        return "".join(subs)
+

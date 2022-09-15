404120ae49ea8cb442bcbcd7e427228aefbcb321
washing1127 <1014585392@qq.com>
2022-09-15 10:50:50 +0800

    copied 0672.py

---
+++ Solutions/0672/0672.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/9/15 10:30
+# File: 0672.py
+# Desc: CV
+
+
+class Solution:
+    def flipLights(self, n: int, presses: int) -> int:
+        seen = set()
+        for i in range(2**4):
+            pressArr = [(i >> j) & 1 for j in range(4)]
+            if sum(pressArr) % 2 == presses % 2 and sum(pressArr) <= presses:
+                status = pressArr[0] ^ pressArr[1] ^ pressArr[3]
+                if n >= 2:
+                    status |= (pressArr[0] ^ pressArr[1]) << 1
+                if n >= 3:
+                    status |= (pressArr[0] ^ pressArr[2]) << 2
+                if n >= 4:
+                    status |= (pressArr[0] ^ pressArr[1] ^ pressArr[3]) << 3
+                seen.add(status)
+        return len(seen)
+

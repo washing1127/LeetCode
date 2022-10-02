549b152b0b979db14392dbda9d7beb2b98492684
washing1127 <1014585392@qq.com>
2022-10-02 16:33:16 +0800

    copied 777.py

---
+++ Solutions/0777/0777.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/10/2 16:24
+# File: 0777.py
+# Desc: CV
+
+
+class Solution:
+    def canTransform(self, start: str, end: str) -> bool:
+        n = len(start)
+        i = j = 0
+        while i < n and j < n:
+            while i < n and start[i] == 'X':
+                i += 1
+            while j < n and end[j] == 'X':
+                j += 1
+            if i < n and j < n:
+                if start[i] != end[j]:
+                    return False
+                c = start[i]
+                if c == 'L' and i < j or c == 'R' and i > j:
+                    return False
+                i += 1
+                j += 1
+        while i < n:
+            if start[i] != 'X':
+                return False
+            i += 1
+        while j < n:
+            if end[j] != 'X':
+                return False
+            j += 1
+        return True
+
+# 作者：LeetCode-Solution
+# 链接：https://leetcode.cn/problems/swap-adjacent-in-lr-string/solution/zai-lrzi-fu-chuan-zhong-jiao-huan-xiang-rjaw8/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。

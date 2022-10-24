1587920f1639eddcf50a7d66e3f0eabb9d011e8a
washing1127 <1014585392@qq.com>
2022-10-24 10:55:50 +0800

    add 0915.py

---
+++ Solutions/0915/0915.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# Author: washing
+# DateTime: 2022/10/24 10:42
+# File: 0915.py
+# Desc: 
+
+
+class Solution:
+    def partitionDisjoint(self, nums: List[int]) -> int:
+        n = len(nums)
+        min_right = [0] * n
+        min_right[-1] = nums[-1]
+        for i in range(n - 2, 0, -1):
+            min_right[i] = min(min_right[i + 1], nums[i])
+
+        max_left = nums[0]
+        for i in range(1, n):
+            if max_left <= min_right[i]:
+                return i
+            max_left = max(max_left, nums[i])
+
